8f19d3f915dacec3566434a8e99d9e6d11b611f2
darshan180800 <darshanrabadiya108@gmail.com>
2024-12-01 21:44:44 -0700

    Added the code

---
+++ Pizzamore Code/script.js
@@ -0,0 +1,13 @@
+// Placeholder for adding interactivity
+document.querySelector('.add-topping-btn').addEventListener('click', () => {
+    alert('Add topping functionality coming soon!');
+  });
+  
+  // Filter functionality (to be developed)
+  const filters = document.querySelectorAll('.filters select');
+  filters.forEach(filter => {
+    filter.addEventListener('change', () => {
+      alert('Filtering...');
+    });
+  });
+  
